2d1d90535bd390d877568cfeb2ea39708e18b906
Maanikya <maanikya.acharya@gmail.com>
2023-06-02 12:00:20 +0530

    Finalized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Editor from "./Components/Editor";
+import useLocalStorage from "./Hooks/useLocalStorage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [html, setHtml] = useLocalStorage("html", "");
+    const [css, setCss] = useLocalStorage("css", "");
+    const [js, setJs] = useLocalStorage("js", "");
+    const [srcDoc, setSrcDoc] = useState("");
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setSrcDoc(`
+            <html>
+                <body>${html}</body>
+                <style>${css}</style>
+                <script>${js}</script>
+            </html>
+        `);
+        }, 250);
+
+        return () => clearTimeout(timeout);
+    }, [html, css, js]);
+
+    return (
+        <>
+            <div className="pane top-pane">
+                <Editor
+                    language="xml"
+                    displayName="HTML"
+                    value={html}
+                    onChange={setHtml}
+                />
+                <Editor
+                    language="css"
+                    displayName="CSS"
+                    value={css}
+                    onChange={setCss}
+                />
+                <Editor
+                    language="javascript"
+                    displayName="Javascript"
+                    value={js}
+                    onChange={setJs}
+                />
+            </div>
+
+            <div className="pane">
+                <iframe
+                    title="output"
+                    srcDoc={srcDoc}
+                    sandbox="allow-scripts"
+                    frameBorder={"0"}
+                    width={"100%"}
+                    height={"100%"}
+                />
+            </div>
+        </>
+    );
 }
 
 export default App;
